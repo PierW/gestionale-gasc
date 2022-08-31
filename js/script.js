7cf93665204e76d0929a2cd1b7d5b8e0e8ead0da
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2022-08-31 10:58:30 +0200

    aggiunto pulsante di stampa e cambiata ux

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ const onGenerateSubmit = (e) => {
     alert('Per piacere compila tutti i campi!');
   } else {
     showSpinner();
+    hideForm();
     // Show spinner for 1 sec
     setTimeout(() => {
       hideSpinner();
@@ -55,6 +56,12 @@ const showSpinner = () => {
   spinner.style.display = 'block';
 };
 
+// Hide Form
+const hideForm = () => {
+  const form = document.getElementById('form-section');
+  form.style.display = 'none';
+};
+
 // Hide spinner
 const hideSpinner = () => {
   const spinner = document.getElementById('spinner');
@@ -63,21 +70,31 @@ const hideSpinner = () => {
 
 // Create save button to download QR code as image
 const createSaveBtn = (saveUrl) => {
+
   const link = document.createElement('a');
-  link.id = 'save-link';
+  link.id = 'create-new-product';
   link.classList =
-    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto mt-5';
+    'bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 rounded w-1/3 m-auto mt-5';
   link.href = saveUrl;
-  link.download = 'qrcode';
-  link.innerHTML = 'Salva QrCode';
+  link.innerHTML = 'Crea nuovo prodotto';
+
+  const link1 = document.createElement('a');
+  link1.id = 'save-link';
+  link1.classList =
+    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto mt-2';
+  link1.href = saveUrl;
+  link1.download = 'qrcode';
+  link1.innerHTML = 'Salva QrCode';
 
   const link2 = document.createElement('a');
   link2.id = 'print-link';
   link2.classList =
     'bg-green-500 hover:bg-green-700 text-white font-bold py-2 rounded w-1/3 m-auto mt-2';
   link2.href = saveUrl;
+  link2.onclick = () => { window.print() };
   link2.innerHTML = 'Stampa QrCode';
-  document.getElementById('generated').append(link, link2);
+
+  document.getElementById('generated').append(link, link1, link2);
 };
 
 hideSpinner();
